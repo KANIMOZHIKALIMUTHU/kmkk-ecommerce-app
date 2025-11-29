7fa3c81516d7a2ceaa74fda5d5b5e126eb6b9a27
KANIMOZHIKALIMUTHU <kmkk2303@gmail.com>
2025-11-29 16:43:31 +0530

    solution

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: 'https://kmkk-ecommerce-app.vercel.app/',
   credentials: true
 }));
 app.use(express.json());
